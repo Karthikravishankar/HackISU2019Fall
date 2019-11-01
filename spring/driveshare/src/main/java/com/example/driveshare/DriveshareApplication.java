f40851330cecb945494d58b33da0930081904eb6
Karthikravishankar <karthik.r.subbarao@gmail.com>
2019-11-01 18:05:50 -0500

    added spring

---
+++ spring/driveshare/src/main/java/com/example/driveshare/DriveshareApplication.java
@@ -0,0 +1,13 @@
+package com.example.driveshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveshareApplication.class, args);
+	}
+
+}
