f5ae1eae517d3cdfec0c5ab1f12db6ecc947631f
Xiuyuan <xiuyuang@iastate.edu>
2019-11-01 20:03:37 -0500

    Minor change on the search page

--- spring/driveshare/src/main/java/com/example/driveshare/Search.java
+++ spring/driveshare/src/main/java/com/example/driveshare/Search.java
@@ -1,12 +1,24 @@
 package com.example.driveshare;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/search")
 public class Search {
 	
-	@RequestMapping("/search")
+	@GetMapping
+	public ResponseEntity<String> MainSearch()
+	{
+		System.out.println("Search page is actived");
+		return new ResponseEntity<>("Welcome to search page" , HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping("/sdf")
 	public String Search()
 	{
 		return "sdf";
