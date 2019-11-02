c3cffcbe239086bea0803bda9414f8a7c5733df3
Ganesh Prasad <ganeshgprasad17@gmail.com>
2019-11-01 23:17:01 -0500

    added search, needs location.

--- FrontEnd/driveshare/app/src/main/java/com/example/driveshare/DriveShareMain.java
+++ FrontEnd/driveshare/app/src/main/java/com/example/driveshare/DriveShareMain.java
@@ -4,16 +4,39 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
 
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriveShareMain extends AppCompatActivity {
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
     private Toolbar toolbar;
     private NavigationView navigation;
+    private Button submit;
+    private Spinner type;
+    private EditText dest;
+    private Double lat, lng;
+
+    /*
+        ToDo:
+        1. Get current location and assign latitude and longitude variables.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +50,48 @@ public class DriveShareMain extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        type = findViewById(R.id.spinner);
+        dest = findViewById(R.id.editText_dest);
+        submit = findViewById(R.id.button_search);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPostRequest();
+            }
+        });
+    }
+
+    private void sendPostRequest(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://" + getString(R.string.ip_address) + ":8080/customerPage/addRequest";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(DriveShareMain.this, "Successfully added request", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        System.out.println(error.fillInStackTrace());
+                        Toast.makeText(DriveShareMain.this, "Failed to create request.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username",getIntent().getStringExtra("username"));
+                params.put("destination",dest.getText().toString());
+                params.put("type", type.getPrompt().toString());
+                params.put("latitude", Double.toString(lat));
+                params.put("longitude", Double.toString(lng));
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 }
