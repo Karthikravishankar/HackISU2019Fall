c06dbda3a607bbaabe3a27ebd3a4514a2b066eb9
Karthikravishankar <karthik.r.subbarao@gmail.com>
2019-11-01 19:10:45 -0500

    first controller

---
+++ spring/driveshare/src/main/java/com/example/driveshare/Login.java
@@ -0,0 +1,30 @@
+package com.example.driveshare;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/login")
+public class Login {
+
+    @GetMapping
+    public ResponseEntity<String> getHome() throws IOException {
+        System.out.println("user logged");
+        return new ResponseEntity<>(
+                "1dayumay",
+                HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/validateLogin", method = RequestMethod.POST)
+    public String getJourneyCoordinates(HttpServletRequest request) throws IOException, InterruptedException {
+        return "";
+    }
+}
