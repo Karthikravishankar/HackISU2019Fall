7a7c93811384613ad3bd724a305539b621587e4c
Xiuyuan <xiuyuang@iastate.edu>
2019-11-01 20:06:23 -0500

    added simple comment for check git

--- spring/driveshare/src/main/java/com/example/driveshare/Search.java
+++ spring/driveshare/src/main/java/com/example/driveshare/Search.java
@@ -13,6 +13,7 @@ public class Search {
 	@GetMapping
 	public ResponseEntity<String> MainSearch()
 	{
+		//simple comment test git
 		System.out.println("Search page is actived");
 		return new ResponseEntity<>("Welcome to search page" , HttpStatus.OK);
 	}
